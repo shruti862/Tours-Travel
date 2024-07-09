003b471c5c13eb9d8dbf52c5879fb756bf24e8a9
shruti862 <shrutiyadav862@gmail.com>
2024-07-09 19:56:12 +0530

    new commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -16,9 +16,8 @@ const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-    secure: process.env.NODE_ENV === 'production',
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
     httpOnly: true,
-    samesite: 'none',
     credentials: 'include'
   };
 
